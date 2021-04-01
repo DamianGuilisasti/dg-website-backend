e17ea462e4bf4241d6ee327aef228035908a1d93
unknown <damianguilisasti@gmail.com>
2021-03-31 22:42:39 -0300

    Update Database URIs

--- src/database.js
+++ src/database.js
@@ -6,9 +6,6 @@ const MONGODB_DATABASE = "dgwebsite";
 //const { MONGODB_HOST, MONGODB_DATABASE } = process.env;
 
 const MONGODB_URI = `mongodb://${MONGODB_HOST}/${MONGODB_DATABASE}`;
-//const MONGODB_URI = `mongodb+srv://Chacky:959688671dD@cluster0.ajddt.mongodb.net/dgwebsite?retryWrites=true&w=majority`;
-
-//mongodb+srv://Chacky:959688671dD@cluster0.ajddt.mongodb.net/dgwebsite?retryWrites=true&w=majority
 
 mongoose
   .connect(MONGODB_URI, {
